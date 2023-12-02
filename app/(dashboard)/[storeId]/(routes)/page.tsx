9ac71167e9f5ec1449cd517f16f0f160ff1a39b7
vgodun <v.godunsoftware@gmail.com>
2023-12-02 18:58:58 +0200

    Dashboard

---
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -0,0 +1,16 @@
+import prismadb from "@/lib/prismadb"
+
+interface DashboardPageProps {
+  params: { storeId: string }
+}
+
+export default async function DashboardPage({ params }: DashboardPageProps) {
+  const store = await prismadb.store.findFirst({ 
+    where: { 
+      id: params.storeId
+     }
+     });
+  return (
+    <div>Active Store: {store?.name}</div>
+  )
+}
