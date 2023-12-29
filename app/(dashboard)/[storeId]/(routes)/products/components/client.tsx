ea66a3d9f35e49dc523f8d6bc2d63c3fa6abfc1b
vgodun <vgodunsoftware@gmail.com>
2023-12-29 20:43:46 +0200

    add products

---
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { Button } from "@/components/ui/button";
+import { DataTable } from "@/components/ui/data-table";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { ProductColumn, columns } from "./columns";
+import { ApiList } from "@/components/ui/api-list";
+
+interface ProductClientProps {
+  data: ProductColumn[];
+}
+
+export default function ProductClient({data}: ProductClientProps) {
+    const router = useRouter();
+    const params= useParams();
+  return (
+    <>
+    <div className="flex items-center justify-between">
+        <Heading 
+        title={`Products (${data.length})`}
+        description="Manage product for your store."
+        />
+        <Button onClick={()=>router.push(`/${params.storeId}/products/new`)}>
+            <Plus className="mr-2 h-4 w-4 " />
+                Add New
+        </Button>
+    </div>
+    <Separator />
+    <DataTable searchKey="name" columns={columns} data={data} />
+    <Heading title="API" description="API calls for Products"/>
+    <Separator />
+    <ApiList entityName="products" entityIdName="productId"/>
+    </>
+  )
+}
